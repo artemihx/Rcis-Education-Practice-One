f2dd7a9e195a1feb2ede8938676b014cc5e8c12b
artemihx <art.pozx1@gmail.com>
2022-11-11 13:55:28 +0700

    УП

--- pr16/bin/z4.dart
+++ pr16/bin/z4.dart
@@ -1,5 +1,23 @@
 import 'dart:io';
 
 void main() {
-  
+  try{
+    stdout.write("Введите a: ");
+    int a = int.parse(stdin.readLineSync()!);
+    stdout.write("Вводите положительные числа: ");
+    int sum = 0;
+    for (int i = 1; i > 0; ){
+      i = int.parse(stdin.readLineSync()!);
+      if(i % a == 0){
+        sum += i;
+      }
+      else if (i % a != 0){
+        continue;
+      }
+    }
+    print("Сумма равна: $sum");
+  }
+  catch(e){
+    print("Неккоректные данные. Завершение программы.");
+  }
 }
